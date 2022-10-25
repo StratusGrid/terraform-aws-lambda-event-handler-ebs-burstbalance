4f145c576bba8d80f441401abb8f10946d8043c3
DJ Kaswa <74205036+DJKaswa@users.noreply.github.com>
2022-10-25 09:57:05 -0500

    Adds pre-commit checks to the repo. --SGPROJ-1564 (#6)
    * Adds pre-commit checks to the repo.
    
    * Delete sync-repo-settings.yaml
    
    * Fixes tfsec warnings raised during pre-commit check.
    
    Co-authored-by: Jose Manuel Motto Sanchez <66962665+JoseMotto-sg@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,18 +1,62 @@
+<!-- BEGIN_TF_DOCS -->
 # terraform-aws-lambda-event-handler-ebs-burstbalance
+
+GitHub: [StratusGrid/terraform-aws-lambda-event-handler-ebs-burstbalance](https://github.com/StratusGrid/terraform-aws-lambda-event-handler-ebs-burstbalance)
+
 This module will deploy a lambda function which will listen for ebs volume creation/deletion events and put/delete BurstBalance alarms for gp2 volumes.
 
-### Example Usage:
-```
+## Example:
+```hcl
 module "ebs_burst_balance_lambda" {
   source  = "StratusGrid/lambda-event-handler-ebs-burstbalance/aws"
-  version = "2.0.0"
+  version = "2.1.0"
   # source  = "github.com/StratusGrid/terraform-aws-lambda-event-handler-ebs-burstbalance"
-  
+
+  region           = var.region
   name_prefix      = var.name_prefix
   name_suffix      = local.name_suffix
+
   unique_name      = "event-handler-ebs-burst-balance"
   sns_alarm_target = aws_sns_topic.infrastructure_alerts.arn
   input_tags       = merge(local.common_tags, {})
 }
-
 ```
+---
+
+## Resources
+
+| Name | Type |
+|------|------|
+| [aws_cloudwatch_event_rule.event](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/cloudwatch_event_rule) | resource |
+| [aws_cloudwatch_event_target.function_target](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/cloudwatch_event_target) | resource |
+| [aws_cloudwatch_log_group.log_group](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/cloudwatch_log_group) | resource |
+| [aws_iam_role.function_role](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role) | resource |
+| [aws_iam_role_policy.function_policy](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role_policy) | resource |
+| [aws_iam_role_policy.function_policy_default](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role_policy) | resource |
+| [aws_kms_key.log_key](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/kms_key) | resource |
+| [aws_lambda_function.function](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lambda_function) | resource |
+| [aws_lambda_permission.allow_cloudwatch_event_trigger](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lambda_permission) | resource |
+
+## Inputs
+
+| Name | Description | Type | Default | Required |
+|------|-------------|------|---------|:--------:|
+| <a name="input_alarm_period"></a> [alarm\_period](#input\_alarm\_period) | Number of seconds for period value of alarm (Less than 300 will result in 'insufficient data' unless you have detailed monitoring enabled!) | `string` | `"300"` | no |
+| <a name="input_alarm_threshold"></a> [alarm\_threshold](#input\_alarm\_threshold) | Float value for alarm threshold (e.g. 75.0) | `string` | `"75.0"` | no |
+| <a name="input_cloudwatch_log_retention_days"></a> [cloudwatch\_log\_retention\_days](#input\_cloudwatch\_log\_retention\_days) | Number of days for retention period of Lambda logs | `string` | `"30"` | no |
+| <a name="input_input_tags"></a> [input\_tags](#input\_input\_tags) | Map of tags to apply to resources | `map(string)` | <pre>{<br>  "Developer": "StratusGrid",<br>  "Provisioner": "Terraform"<br>}</pre> | no |
+| <a name="input_kms_log_key_deletion_window"></a> [kms\_log\_key\_deletion\_window](#input\_kms\_log\_key\_deletion\_window) | Duration (in day) of kms key created, default is 30 | `number` | n/a | yes |
+| <a name="input_lambda_tracing_option"></a> [lambda\_tracing\_option](#input\_lambda\_tracing\_option) | Lambda Tracing option whehter to sample and trace a subset of incoming requests with AWS X-Ray. | `string` | `"Active"` | no |
+| <a name="input_name_prefix"></a> [name\_prefix](#input\_name\_prefix) | String to prefix on object names | `string` | `""` | no |
+| <a name="input_name_suffix"></a> [name\_suffix](#input\_name\_suffix) | String to append to object names. This is optional, so start with dash if using | `string` | `""` | no |
+| <a name="input_sns_alarm_target"></a> [sns\_alarm\_target](#input\_sns\_alarm\_target) | ARN for sns alarm to be targeted for performance alerts | `string` | `""` | no |
+| <a name="input_unique_name"></a> [unique\_name](#input\_unique\_name) | Unique string to describe resources. E.g. 'ebs-append' would make <prefix><name>(type)<suffix> | `string` | n/a | yes |
+
+## Outputs
+
+No outputs.
+
+---
+
+<span style="color:red">Note:</span> Manual changes to the README will be overwritten when the documentation is updated. To update the documentation, run `terraform-docs -c .config/.terraform-docs.yml .`
+<!-- END_TF_DOCS -->
--- config.tf
+++ config.tf
@@ -1,5 +1,4 @@
 #Event rule to direct events to the Lambda Function
-
 resource "aws_cloudwatch_event_rule" "event" {
   name        = "${var.name_prefix}-${var.unique_name}-rule${var.name_suffix}"
   description = "Pattern of events to forward to targets"
@@ -25,41 +24,45 @@ PATTERN
 
 #Target to direct event at function
 resource "aws_cloudwatch_event_target" "function_target" {
-  rule = aws_cloudwatch_event_rule.event.name
+  rule      = aws_cloudwatch_event_rule.event.name
   target_id = "${var.name_prefix}-${var.unique_name}-target${var.name_suffix}"
-  arn = aws_lambda_function.function.arn
+  arn       = aws_lambda_function.function.arn
 }
 
 #Permission to allow event trigger
 resource "aws_lambda_permission" "allow_cloudwatch_event_trigger" {
-  statement_id = "TrustCWEToInvokeMyLambdaFunction"
-  action = "lambda:InvokeFunction"
+  statement_id  = "TrustCWEToInvokeMyLambdaFunction"
+  action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.function.function_name
-  principal = "events.amazonaws.com"
-  source_arn = aws_cloudwatch_event_rule.event.arn
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.event.arn
 }
 
 #Automatic packaging of code
 data "archive_file" "function_code" {
-  type = "zip"
-  source_dir = "${path.module}/function_code"
+  type        = "zip"
+  source_dir  = "${path.module}/function_code"
   output_path = "${path.module}/function_code_zipped/function_code.zip"
 }
 
 #Function to process event
 resource "aws_lambda_function" "function" {
-  filename = data.archive_file.function_code.output_path
+  filename         = data.archive_file.function_code.output_path
   source_code_hash = filebase64sha256(data.archive_file.function_code.output_path)
-  function_name = "${var.name_prefix}-${var.unique_name}-function${var.name_suffix}"
-  role = aws_iam_role.function_role.arn
-  handler = "main.handler"
-  runtime = "python3.9"
-  timeout = "10"
+  function_name    = "${var.name_prefix}-${var.unique_name}-function${var.name_suffix}"
+  role             = aws_iam_role.function_role.arn
+  handler          = "main.handler"
+  runtime          = "python3.9"
+  timeout          = "10"
+  tracing_config {
+    mode = var.lambda_tracing_option
+  }
+
   environment {
     variables = {
       sns_alarm_target = var.sns_alarm_target
-      alarm_threshold = var.alarm_threshold
-      alarm_period = var.alarm_period
+      alarm_threshold  = var.alarm_threshold
+      alarm_period     = var.alarm_period
     }
   }
   lifecycle {
@@ -93,10 +96,10 @@ EOF
 
 #Default policy for Lambda to be executed and put logs in Cloudwatch
 resource "aws_iam_role_policy" "function_policy_default" {
-name = "${var.name_prefix}-${var.unique_name}-policy-default${var.name_suffix}"
-role = aws_iam_role.function_role.id
+  name = "${var.name_prefix}-${var.unique_name}-policy-default${var.name_suffix}"
+  role = aws_iam_role.function_role.id
 
-policy = <<EOF
+  policy = <<EOF
 {
   "Version": "2012-10-17",
   "Statement": [
@@ -104,7 +107,7 @@ policy = <<EOF
       "Sid": "AllowListCloudWatchLogGroups",
       "Effect": "Allow",
       "Action": "logs:DescribeLogStreams",
-      "Resource": "arn:aws:logs:us-east-1:*:*"
+      "Resource": "${aws_cloudwatch_log_group.log_group.arn}:*"
     },
     {
       "Sid": "AllowCreatePutLogGroupsStreams",
@@ -115,8 +118,8 @@ policy = <<EOF
           "logs:CreateLogGroup"
       ],
       "Resource": [
-          "arn:aws:logs:us-east-1:*:log-group:/aws/lambda/${aws_lambda_function.function.function_name}",
-          "arn:aws:logs:us-east-1:*:log-group:/aws/lambda/${aws_lambda_function.function.function_name}:log-stream:*"
+          aws_cloudwatch_log_group.log_group.arn,
+          "${aws_cloudwatch_log_group.log_group.arn}:log-stream:*"
       ]
     }
   ]
@@ -126,11 +129,12 @@ EOF
 }
 
 #Policy for additional Permissions for Lambda Execution
+#tfsec:ignore:aws-iam-no-policy-wildcards --Ignores warning on usage of Wildcards as changing them will require a refactor of the policy. An issue has been raised for this.
 resource "aws_iam_role_policy" "function_policy" {
-name = "${var.name_prefix}-${var.unique_name}-policy${var.name_suffix}"
-role = aws_iam_role.function_role.id
+  name = "${var.name_prefix}-${var.unique_name}-policy${var.name_suffix}"
+  role = aws_iam_role.function_role.id
 
-policy = <<EOF
+  policy = <<EOF
 {
   "Version": "2012-10-17",
   "Statement": [
@@ -146,7 +150,7 @@ policy = <<EOF
       "Sid": "AllowEc2Actions",
       "Effect": "Allow",
       "Action": [
-          "ec2:DescribeVolume*"
+          "ec2:DescribeVolumes"
       ],
       "Resource": "*"
     }
@@ -161,7 +165,14 @@ resource "aws_cloudwatch_log_group" "log_group" {
   name = "/aws/lambda/${aws_lambda_function.function.function_name}"
 
   retention_in_days = var.cloudwatch_log_retention_days
+  kms_key_id        = aws_kms_key.log_key.arn
 
   tags = local.common_tags
 }
 
+# CloudWatch Logs encryption key
+resource "aws_kms_key" "log_key" {
+  description             = "KMS key for encryption of cloudwatch logs generated by lambda trigger"
+  enable_key_rotation     = true
+  deletion_window_in_days = var.kms_log_key_deletion_window
+}
